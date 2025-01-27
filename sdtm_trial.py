392e5a26ffd8ee247831f06f9ce913e2bdd11f28
trainingaryamt24042 <trainingarya@gmail.com>
2025-01-27 17:33:24 +0530

    Update sdtm_trial.py

--- sdtm_trial.py
+++ sdtm_trial.py
@@ -9,6 +9,7 @@ jdbc_url = "jdbc:postgresql://w3.training5.modak.com:5432/postgres"
 table_name = "sdtm_dm"
 output_filtered_table = "sdtm_dm_filtered"
 output_sum_table = "sdtm_dm_age_sum"
+output_sum_avg = "sdtm_avg_age_race"
 date_diff_table = "sdtm_date_diff_table"
 properties = {"user": "mt24042", "password": "mt24042@m06y24", "driver": "org.postgresql.Driver"}
 
@@ -19,6 +20,9 @@ def avg_age_per_race(df, column_name):
     return df.groupBy("race").agg(avg("age").alias("avg_age"))
 
 df = spark.read.jdbc(url=jdbc_url, table=table_name, properties=properties)
+
+avg_age_df = avg_age_per_race(df, "race")
+avg_age_df.write.jdbc(url=jdbc_url, table=output_sum_avg, mode="overwrite", properties=properties)
 
 df_birthdate = skew_birthdate(df, "brthdt")
 df_birthdate.write.jdbc(url=jdbc_url, table=output_filtered_table, mode="overwrite", properties=properties)
