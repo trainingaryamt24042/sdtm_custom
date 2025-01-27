845f4d8cd456bd5ba9ceebca7b97f8b047d14a3a
trainingaryamt24042 <trainingarya@gmail.com>
2025-01-27 17:25:09 +0530

    Update sdtm_trial.py

--- sdtm_trial.py
+++ sdtm_trial.py
@@ -12,16 +12,21 @@ output_sum_table = "sdtm_dm_age_sum"
 date_diff_table = "sdtm_date_diff_table"
 properties = {"user": "mt24042", "password": "mt24042@m06y24", "driver": "org.postgresql.Driver"}
 
+def skew_birthdate(df, column_name):
+    return df.withColumn(column_name, expr(f"date_add({column_name}, 5)"))
+
+def avg_age_per_race(df, column_name):
+    return df.groupBy("race").agg(avg("age").alias("avg_age"))
+
 df = spark.read.jdbc(url=jdbc_url, table=table_name, properties=properties)
 
 filtered_df = df.where((df.age > 70) & (df.race == 'Asian'))
-
 filtered_df.write.jdbc(url=jdbc_url, table=output_filtered_table, mode="overwrite", properties=properties)
 
-age_sum = filtered_df.select(_sum(col("age")).alias("total_age"))
+date_diff = df.withColumn("date_diff", datediff(df["rfendtc"], df["rfstdtc"]))
+date_diff.write.jdbc(url=jdbc_url, table=date_diff_table, mode="overwrite", properties=properties)
 
+age_sum = filtered_df.select(_sum(col("age")).alias("total_age"))
 age_sum.write.jdbc(url=jdbc_url, table=output_sum_table, mode="overwrite", properties=properties)
 
-date_diff = df.datediff(df.rfstdtc,df.rfendtc)
-date_diff.write.jdbc(url=jdbc_url, table=date_diff_table, mode="overwrite", properties=properties)
 spark.stop()
