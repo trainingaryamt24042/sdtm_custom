c43faae844d5f24311af5de75a734daf41d85dfd
trainingaryamt24042 <trainingarya@gmail.com>
2025-01-27 15:40:44 +0530

    Update sdtm_trial.py

--- sdtm_trial.py
+++ sdtm_trial.py
@@ -9,6 +9,7 @@ jdbc_url = "jdbc:postgresql://w3.training5.modak.com:5432/postgres"
 table_name = "sdtm_dm"
 output_filtered_table = "sdtm_dm_filtered"
 output_sum_table = "sdtm_dm_age_sum"
+date_diff_table = "sdtm_date_diff_table"
 properties = {"user": "mt24042", "password": "mt24042@m06y24", "driver": "org.postgresql.Driver"}
 
 df = spark.read.jdbc(url=jdbc_url, table=table_name, properties=properties)
@@ -20,4 +21,7 @@ filtered_df.write.jdbc(url=jdbc_url, table=output_filtered_table, mode="overwrit
 age_sum = filtered_df.select(_sum(col("age")).alias("total_age"))
 
 age_sum.write.jdbc(url=jdbc_url, table=output_sum_table, mode="overwrite", properties=properties)
+
+date_diff = df.datediff(df.rfstdtc,df.rfendtc)
+date_diff.write.jdbc(url=jdbc_url, table=date_diff_table, mode="overwrite", properties=properties)
 spark.stop()
